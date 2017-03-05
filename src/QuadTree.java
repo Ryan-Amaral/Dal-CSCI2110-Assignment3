1c1dcb42dccdc96e7ea2711c164d383772598171
Ryan-Amaral <ryan_amaral@live.com>
2017-03-05 12:27:50 -0400

    started implementing quad tree, deleted bounds

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -1,4 +1,46 @@
+import java.util.ArrayList;
 
-public class QuadTree {
+/**
+ * An implementation of TwoDimDictionary that uses a quadree to be able
+ * to perform query and count operation in O(log base 4 of n).
+ * 
+ * Assumptions/Restrictions: 
+ * 
+ * Noteworthy Features:
+ * 
+ * @author Ryan Amaral
+ *
+ */
+public class QuadTree implements TwoDimDictionary {
+
+    @Override
+    public void insert(Point p) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int count(Rectangle b) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void query(ArrayList<Point> s, Rectangle b) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void display() {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
